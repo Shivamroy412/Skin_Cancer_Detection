99d6df79820e55d7495210ca3d425d0ef4869aae
root <roy.shivam412@gmail.com>
2022-04-07 00:07:04 +0000

    Modified cross validation script with absolute Input Path and shuffling

--- src/create_folds.py
+++ src/create_folds.py
@@ -2,15 +2,18 @@ import os
 import pandas as pd
 from sklearn import model_selection
 
+DATA_PATH = "/kaggle/input/siim-isic-melanoma-classification"
+
+
 if __name__ == "__main__":
 
-    df = pd.read_csv("../input/train.csv")
+    df = pd.read_csv(os.path.join(DATA_PATH, "train.csv"))
     df["kfold"] = -1
     df = df.sample(frac=1).reset_index(drop = True)
     
     y = df.target.values
 
-    kfold_model = model_selection.StratifiedKFold(n_splits = 10)
+    kfold_model = model_selection.StratifiedKFold(n_splits = 10, shuffle = True, random_state = 49 )
 
     for fold_, (train_idx_, valid_idx_) in enumerate(kfold_model.split(X=df, y=y)):
         df.loc[valid_idx_ , "kfold"] = fold_
