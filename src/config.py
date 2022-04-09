258921bc5962512bd7616712d28daf59ce0b8651
root <roy,shivam412@gmail.com>
2022-04-09 09:10:19 +0000

    Defined absolute paths in config file, made changes to code to leverage Kaggle environment

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import os
+
+#Defining absolute Paths for directories, the os.path.sep method below adds the forward slash
+#required for defining absolute Paths
+
+DATA_PATH = os.path.join(os.path.sep , "kaggle","input","siim-isic-melanoma-classification")
+
+INPUT_PATH = os.path.join(os.path.sep , "kaggle","working","Skin_Cancer_Detection","input")
+
--- src/create_folds.py
+++ src/create_folds.py
@@ -1,13 +1,11 @@
 import os
 import pandas as pd
 from sklearn import model_selection
-
-DATA_PATH = "/kaggle/input/siim-isic-melanoma-classification"
-
+import config
 
 if __name__ == "__main__":
 
-    df = pd.read_csv(os.path.join(DATA_PATH, "train.csv"))
+    df = pd.read_csv(os.path.join(config.DATA_PATH, "train.csv"))
     df["kfold"] = -1
     df = df.sample(frac=1).reset_index(drop = True)
     
@@ -18,6 +16,7 @@ if __name__ == "__main__":
     for fold_, (train_idx_, valid_idx_) in enumerate(kfold_model.split(X=df, y=y)):
         df.loc[valid_idx_ , "kfold"] = fold_
 
-    df.to_csv("../input/train_folds.csv", index = False)
+    os.makedirs(config.INPUT_PATH, exist_ok = True)
+    df.to_csv(os.path.join(config.INPUT_PATH, "train_folds.csv"), index = False)
     
     print(df.kfold.value_counts())
