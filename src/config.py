cea863d3bc62506a7e1d5c3c735a57ffac80480c
Shivam Roy <roy.shivam412@gmail.com>
2022-05-06 14:00:12 +0530

    Modified code to train on local machines easily and make the code platform agnostic

--- src/config.py
+++ src/config.py
@@ -3,7 +3,10 @@ import os
 #Defining absolute Paths for directories, the os.path.sep method below adds the forward slash
 #required for defining absolute Paths
 
-DATA_PATH = os.path.join(os.path.sep , "kaggle","input","siim-isic-melanoma-classification")
+DATA_PATH = os.path.join(os.path.sep, "kaggle","input","siim-isic-melanoma-classification")
 
-INPUT_PATH = os.path.join(os.path.sep , "kaggle","working","Skin_Cancer_Detection","input")
+INPUT_PATH = os.path.join(os.path.sep, "kaggle","working","Skin_Cancer_Detection","input")
 
+#When training locally the folder sturctures are different
+training_local = True
+LOCAL_ROOT_PATH = os.path.join(os.path.sep, "Users", "ShivamRoy", "Development", "Python")
--- src/create_folds.py
+++ src/create_folds.py
@@ -5,7 +5,13 @@ import config
 
 if __name__ == "__main__":
 
-    df = pd.read_csv(os.path.join(config.DATA_PATH, "train.csv"))
+    if config.training_local:
+        input_path = os.path.join(config.LOCAL_ROOT_PATH, config.INPUT_PATH[1:])
+        data_path = os.path.join(config.LOCAL_ROOT_PATH, config.DATA_PATH[1:])
+        #Sliced the path above to remove the leading '/' seperator
+
+
+    df = pd.read_csv(os.path.join(data_path, "train.csv"))
     df["kfold"] = -1
     df = df.sample(frac=1).reset_index(drop = True)
     
@@ -16,7 +22,8 @@ if __name__ == "__main__":
     for fold_, (train_idx_, valid_idx_) in enumerate(kfold_model.split(X=df, y=y)):
         df.loc[valid_idx_ , "kfold"] = fold_
 
-    os.makedirs(config.INPUT_PATH, exist_ok = True)
-    df.to_csv(os.path.join(config.INPUT_PATH, "train_folds.csv"), index = False)
+    
+    os.makedirs(input_path, exist_ok = True)
+    df.to_csv(os.path.join(input_path, "train_folds.csv"), index = False)
     
     print(df.kfold.value_counts())
