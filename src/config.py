6967a66d4d9b5773cba4116ff2c2330e255329fd
Shivam Roy <roy.shivam412@gmail.com>
2022-05-29 00:18:07 +0530

    Modified Dataset class to work for torchvision instead of albumentations

--- src/config.py
+++ src/config.py
@@ -15,10 +15,13 @@ DATA_PATH = os.path.join(os.path.sep, "kaggle","input","siim-isic-melanoma-class
 
 INPUT_PATH = os.path.join(os.path.sep, "kaggle","working","Skin_Cancer_Detection","input")
 
+SRC_PATH = os.path.join(os.path.sep, "kaggle","working","Skin_Cancer_Detection","src")
+
 if training_local:
     LOCAL_ROOT_PATH = os.path.join(os.path.sep, "Users", "ShivamRoy", "Development", "Python")
 
     INPUT_PATH = os.path.join(LOCAL_ROOT_PATH, INPUT_PATH[1:])
     DATA_PATH = os.path.join(LOCAL_ROOT_PATH, DATA_PATH[1:])
+    SRC_PATH = os.path.join(LOCAL_ROOT_PATH, SRC_PATH[1:])
     #Sliced the path above to remove the leading '/' seperator
 
--- src/dataset.py
+++ src/dataset.py
@@ -1,14 +1,15 @@
+import os
 import pandas as pd
 import numpy as np
-import config
-import os
 from PIL import Image
 import torch
+from torchvision import transforms as T
+import config
 
 
 class SkinCancerDataset:
 
-    def __init__(self, folds: list, augmentations=None, channel_first = True):
+    def __init__(self, folds: list, transforms=None, channel_first = True):
         
         df = pd.read_csv(os.path.join(config.INPUT_PATH,"train_folds.csv"))[["image_name", "target", "kfold"]]
         df = df.loc[df.kfold.isin(folds)].reset_index(drop= True)
@@ -17,8 +18,8 @@ class SkinCancerDataset:
         self.image_paths = [os.path.join(config.INPUT_PATH, "processed_img_train", 
                                  file_name + ".jpg") for file_name in image_names]
 
-        self.target = df.target.to_numpy()
-        self.augmentations = augmentations
+        self.target = torch.tensor(df.target.to_numpy())
+        self.transforms = transforms
         self.channel_first = channel_first 
         #Whether channel dimension is mentioned at the beginning of the image array
 
@@ -33,13 +34,14 @@ class SkinCancerDataset:
         image = Image.open(self.image_paths[idx])
         image = np.array(image)
 
-        if self.augmentations:
-            augmentation_dict = self.augmentations(image = image)
-            #This returns a dictionary with image as one of the keys
-            image = augmentation_dict["image"]
+        if not self.transforms:
+            self.transforms = T.Compose([T.ToTensor()])
+        
+        image = self.transforms(image)
 
-        if self.channel_first:
-            image = np.transpose(image, (2, 0 , 1)).astype(np.float32)
+        return image, target
 
-        return {"image": torch.tensor(image), 
-                "target": torch.tensor(target)}
+#Tests
+#data = SkinCancerDataset([0])
+#print(data[0])
+#print(data[0][0].shape)
